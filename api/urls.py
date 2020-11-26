dc575d47cf162d302252b2b1fdd9df0ad0e56a95
KzmMthr <gurkinnn@yandex.ru>
2020-11-26 11:08:48 +0300

    rewrite regexp in urls

--- api/urls.py
+++ api/urls.py
@@ -9,9 +9,9 @@ v1_router.register('titles', TitleViewSet, basename='titles')
 v1_router.register('categories', CategoryViewSet, basename='categories')
 v1_router.register('genres', GenreViewSet, basename='genres')
 v1_router.register('users', UserViewSet, basename='users')
-v1_router.register(r'titles/(?P<title_id>[^/.]+)/reviews', ReviewsViewSet, basename='reviews')
+v1_router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewsViewSet, basename='reviews')
 v1_router.register(
-    r'titles/(?P<title_id>[^/.]+)/reviews/(?P<review_id>[^/.]+)/comments',
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentsViewSet, basename='comments'
 )
 
