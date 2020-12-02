c338a97f76ea25bb1bd4e50433f489185d94b0b0
DaniilDDDDD <daniilpanyushin@yandex.ru>
2020-12-02 16:53:14 +0300

    Fix after third code review

--- users/models.py
+++ users/models.py
@@ -7,7 +7,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-
     class Roles(models.TextChoices):
         ADMIN = 'admin', _('admin')
         MODERATOR = 'moderator', _('moderator')
@@ -15,8 +14,6 @@ class CustomUser(AbstractUser):
 
     role = models.CharField(max_length=100, choices=Roles.choices, default=Roles.USER)
     bio = models.TextField(blank=True)
-    is_admin = models.BooleanField(default=False)
-    is_moderator = models.BooleanField(default=False)
 
     confirmation_code = models.CharField(_('confirmation_code'), max_length=10, null=True, blank=True)
 
@@ -28,3 +25,11 @@ class CustomUser(AbstractUser):
     )
 
     objects = CustomUserManager()
+
+    @property
+    def is_moderator(self):
+        return self.role == self.Roles.MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == self.Roles.ADMIN
